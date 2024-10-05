d32da17fa1b130ee131c47bda196d91f5d6092d3
Everest Young <94253253+YazalskiGamings@users.noreply.github.com>
2024-10-04 21:14:45 -0400

    Update LinearRegressionScratch.R

--- linear regression/LinearRegressionScratch.R
+++ linear regression/LinearRegressionScratch.R
@@ -5,15 +5,14 @@ library(scatterplot3d)
 
 fitness = read_csv(".../data.csv") #import fitness csv
 
-cor(fitness) #Maxpulse -> pulse are correlated. That makes sense. Avoid Multicolinearity, use Maxpulse only
+cor(fitness) #Maxpulse & Pulse are correlated. That makes sense. Avoid Multicolinearity, so use Maxpulse only
 fitness = fitness %>% select(-c(Pulse))
 
-fitness$Calories = ifelse(is.na(fitness$Calories) == T, median(fitness$Calories, na.rm = T), fitness$Calories) #impute na for median because skewed
-hist(fitness$Calories) # Skewed, let's try to do a transformation, so we limit outliers
-fitness$Calories = log(fitness$Calories) # OLS is invariant -> so good to log transform to keep meaning
+fitness$Calories = ifelse(is.na(fitness$Calories) == T, median(fitness$Calories, na.rm = T), fitness$Calories) #impute median for the na because skewed
+hist(fitness$Calories) # Skewed, let's try to do a log transformation, so we limit outliers
+fitness$Calories = log(fitness$Calories) # OLS is invariant -> so good to log transform
 
 Y = as.matrix(fitness$Duration) # Y = 169 x 1
-
 X = as.matrix(fitness[,-1]) # X = 169 x 2 
 X = cbind(intercept = rep(1, nrow(fitness)),X) #add intercept column 169 x 3
 N = nrow(fitness)
